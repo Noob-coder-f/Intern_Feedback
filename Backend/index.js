16d276b9388e6e5f91d4fe364e663ed8e78194f1
Priyanka <priya.dixit.25032002@gmail.com>
2025-10-30 19:23:41 +0530

    Committed changes for POST User Registration

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import authRoutes from "./Route/authRoutes.js";
 const app = express();
 const port = 3000;
 import dotenv from 'dotenv';
@@ -18,8 +19,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use("/user", authRoutes);
+
 connectDB();
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
