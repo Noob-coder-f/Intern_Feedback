94317440a8fab7a2ec3e5225ad277e437a97eee6
Sameersingh001 <mrsameersingh001gmail.com>
2025-10-31 18:18:16 +0530

    ds

--- Backend/Route/feedbackRoutes.js
+++ Backend/Route/feedbackRoutes.js
@@ -1,8 +1,7 @@
 import express from "express";
-import { createFeedback } from "../Controllers/FeedbackController.js";
 
 const router = express.Router();
 
-router.post("/feedback", createFeedback);
+
 
 export default router;
